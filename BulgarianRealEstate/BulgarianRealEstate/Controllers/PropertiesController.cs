910ca4c5c4e56a56dc6aba24463202dec9eabb5d
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-19 14:46:56 +0200

    Added Property Controller, Model and View

---
+++ BulgarianRealEstate/BulgarianRealEstate/Controllers/PropertiesController.cs
@@ -0,0 +1,18 @@
+﻿using BulgarianRealEstate.Views.RealEstates;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulgarianRealEstate.Controllers
+{
+    public class PropertiesController : Controller
+    {
+        public IActionResult Add() => View();
+
+        [HttpPost]
+        public IActionResult Add(AddPropertyFormModel property) => View();
+
+    }
+}
---
+++ BulgarianRealEstate/BulgarianRealEstate/Models/Properties/AddPropertyFormModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulgarianRealEstate.Views.RealEstates
+{
+    public class AddPropertyFormModel
+    {
+        public int Id { get; set; }
+
+        public int Size { get; set; }
+
+        public int Floor { get; set; }
+
+        public int TotalNumberOfFloor { get; set; }
+
+        public int Year { get; set; }
+
+        public int DistrictId { get; set; }
+
+        public int PropertyTypeId { get; set; }
+
+        public int BuildingTypeId { get; set; }
+
+        public int Price { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}

3c5da2c4643a34947eed4541002673772bcf3424
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-22 18:44:29 +0200

    Implemented model state

--- BulgarianRealEstate/BulgarianRealEstate/Models/Properties/AddPropertyFormModel.cs
+++ BulgarianRealEstate/BulgarianRealEstate/Models/Properties/AddPropertyFormModel.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using static BulgarianRealEstate.Data.DataConstants;
 
 namespace BulgarianRealEstate.Models.Properties
 {
     public class AddPropertyFormModel
-    { 
+    {
+        [Range(SizeMinValue, SizeMaxValue)]
         public int Size { get; init; }
 
+        [Range(FloorMinValue, FloorMaxValue)]
         public int Floor { get; init; }
 
+        [Range(TotalNumberOfFloorMinValue, TotalNumberOfFloorMaxValue)]
         [Display(Name = "Total number of floor")]
         public int TotalNumberOfFloor { get; init; }
 
+        [Range(YearMinValue, YearMaxValue)]
         public int Year { get; init; }
 
         [Display(Name = "District")]
@@ -26,8 +31,11 @@ namespace BulgarianRealEstate.Models.Properties
         [Display(Name = "Building type")]
         public int BuildingTypeId { get; init; }
 
+        [Range(PriceMinValue, PriceMaxValue)]
         public int Price { get; init; }
 
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
         public string Description { get; init; }
 
         public IEnumerable<PropertyTypeViewModel> PropertyTypes { get; set; }

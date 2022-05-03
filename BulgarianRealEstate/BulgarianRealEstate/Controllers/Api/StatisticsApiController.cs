e1919987a997e17c9830e3e046d63f3307ae86d9
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-05-03 22:45:59 +0300

    Implemented ApiController

---
+++ BulgarianRealEstate/BulgarianRealEstate/Controllers/Api/StatisticsApiController.cs
@@ -0,0 +1,37 @@
+﻿using BulgarianRealEstate.Data;
+using BulgarianRealEstate.Models.Api.Statistics;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulgarianRealEstate.Controllers.Api
+{
+    [ApiController]
+    [Route("api/statistics")]
+    public class StatisticsApiController : ControllerBase
+    {
+        private readonly RealEstateDbContext data;
+        public StatisticsApiController(RealEstateDbContext data)
+        {
+            this.data = data;
+        }
+
+        [HttpGet]
+        public StatisticsResponseModel GetStatistics()
+        {
+            var totalProperties = this.data.Properties.Count();
+            var totalUsers = this.data.Users.Count();
+
+            return new StatisticsResponseModel
+            {
+                TotalProperties = totalProperties,
+                TotalUsers = totalUsers,
+                TotalSales = 0
+            };
+        }
+
+
+    }
+}
---
+++ BulgarianRealEstate/BulgarianRealEstate/Models/Api/Statistics/StatisticsResponseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulgarianRealEstate.Models.Api.Statistics
+{
+    public class StatisticsResponseModel
+    {
+        public int TotalProperties { get; init; }
+
+        public int TotalUsers { get; init; }
+
+        public int TotalSales { get; init; }
+
+    }
+}
